c98446686b8eaea7679878781f080cba4e2396ff
photonnn <wasjiri@gmail.com>
2022-11-23 22:32:20 +0100

    Add root file

---
+++ src/root.js
@@ -0,0 +1,11 @@
+/*
+  This file serves as a place for global constants and functions.
+
+  Orginally came to be, because jest wouldn't work well with import 'style.css'.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export const globalConsts = {
+  MINIMUM_SHIP_LENGTH: 1,
+  MAXIMUM_SHIP_LENGTH: 4,
+};
