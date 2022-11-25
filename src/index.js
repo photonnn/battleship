08bd2b5838aaaa9a25d6cb4ecc28d73da10b4f26
photonnn <wasjiri@gmail.com>
2022-11-25 21:14:32 +0100

    Add and improve the TO DO list

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './style.css';
   TO DO LIST:
 
   Change the way coordinates work from an array [x, y] to something more readable.
+    # Maybe an object with x and y property
+
+  Add the notifation that calls out that the ship was or was nor succesfully placed.
+    # We have tests for that. Might add an object to log things though.
+
+  Fix legalMoves functions to make a legalMoves based on the ship length
+    and the state of the board.
+    # Next on the menu
 
 */
