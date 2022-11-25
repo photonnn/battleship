d470327e90150e1e778a34a5701ff5c36273445f
photonnn <wasjiri@gmail.com>
2022-11-25 20:12:10 +0100

    Reimplement functions outside of return object

--- src/gameboard.js
+++ src/gameboard.js
@@ -16,7 +16,7 @@ function isSuitable(board, ship, coordinates, direction) {
   const y = coordinates[1];
 
   while (shipLength > 0) {
-    if ((board[x].length - 1) <= (x + i) || (board[x][y].length - 1) <= (y + j) // RETHINK THIS
+    if ((board[x].length - 1) < (x + i) || (board[x][y].length - 1) < (y + j) // RETHINK THIS
     || board[x + j][y + i] !== 0) {
       return false;
     }
@@ -41,35 +41,58 @@ export function createGameboard(width, height) {
       height -> board height
   */
 
-  return {
-    board: Array(width).fill(0).map(() => Array(height).fill(0)),
-    resetBoard() {
-      this.board = Array(width).fill(0).map(() => Array(height).fill(0));
-    },
-    place(ship, coordinates, direction) {
-      /* Places a ship on given coordinates in given direction if it is suitable
+  function resetBoard() {
+    this.board = Array(width).fill(0).map(() => Array(height).fill(0));
+  }
+
+  function place(ship, coordinates, direction) {
+    /* Places a ship on given coordinates in given direction if it is suitable
+
+      Args:
+      ship -> Object
+      coordinates -> Array -> [x, y]
+      direction -> String -> 'X' or 'Y'
+    */
+    if (isSuitable(this.board, ship, coordinates, direction)) {
+      let shipLength = ship.length;
+      let i = 0;
+      let j = 0;
+      while (shipLength > 0) {
+        this.board[coordinates[0] + i][coordinates[1] + j] = ship;
+
+        if (direction === 'x') {
+          j += 1;
+        } else {
+          i += 1;
+        }
+
+        shipLength -= 1;
+      }
+    }
+  }
+
+  function receiveAttack(coordinates) {
+    /* Determine if an attack hit a ship and if so, then send hit to correct ship
 
         Args:
-        ship -> Object
-        coordinates -> Array -> [x, y]
-        direction -> String -> 'X' or 'Y'
+        coordinates -> String -> [x, y]
       */
-      if (isSuitable(this.board, ship, coordinates, direction)) {
-        let shipLength = ship.length;
-        let i = 0;
-        let j = 0;
-        while (shipLength > 0) {
-          this.board[coordinates[0] + i][coordinates[1] + j] = ship;
+    if (this.board[coordinates[0]][coordinates[1]] === 0
+          || this.board[coordinates[0][coordinates[1] === 1]]) {
+      // 0 denotes a blank block, 1 denotes an already attacked block
+      this.board[coordinates[0]][coordinates[1]] = 1;
+      return null;
+    }
+    const ship = this.board[coordinates[0]][coordinates[1]];
+    ship.hit();
+    this.board[coordinates[0]][coordinates[1]] = 1; // CHANGE LATER TO SOMETHING ELSE
+    return null;
+  }
 
-          if (direction === 'x') {
-            j += 1;
-          } else {
-            i += 1;
-          }
-
-          shipLength -= 1;
-        }
-      }
-    },
+  return {
+    board: Array(width).fill(0).map(() => Array(height).fill(0)),
+    resetBoard,
+    place,
+    receiveAttack,
   };
 }
