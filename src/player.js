5e4b2ac8c43bf1978b4efcc12b4f72b6fed4e956
photonnn <wasjiri@gmail.com>
2022-11-25 21:13:15 +0100

    Add work in progress AIMove function

--- src/player.js
+++ src/player.js
@@ -1,3 +1,5 @@
+import { globalConsts } from './root';
+
 // eslint-disable-next-line import/prefer-default-export
 export function createPlayer(id) {
   /* Factory function for the player object
@@ -6,9 +8,36 @@ export function createPlayer(id) {
     id -> String -> 'User' or 'AI'
   */
 
-  function makeAIMove() {
-    /* Make a random move for an AI */
+  function getLegalMoves(board) {
+    /* Return an array filled with coordinates of legal moves
+
+      Args:
+      board -> Object -> A gameboard, see gameboard.js
+    */
+    const legalMoves = [];
+    for (let y = 0; y < board.length; y += 1) {
+      for (let x = 0; x < board[x].length; x += 1) {
+        if (board[y][x] === 0) {
+          legalMoves.push([y, x]);
+        }
+      }
+    }
+    return legalMoves;
   }
+
+  function chooseAIMoveCoordinates() {
+    /* Choose a random coordinats for an AI based on length of array of legal moves */
+    const legalMoves = getLegalMoves();
+    const maxValue = legalMoves.length - 1;
+    return legalMoves[Math.floor(Math.random() * (maxValue + 1))];
+  }
+
+  function makeAIMove(board, ship) {
+    /* Make a random move for an AI */
+    const moveCoordinates = chooseAIMoveCoordinates();
+    board.place(ship, moveCoordinates, globalConsts.SHIP_DIRECTION);
+  }
+
   return {
     id,
     makeAIMove,
