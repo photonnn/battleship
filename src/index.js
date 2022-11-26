37c9381ee4b06fbcb55e2e871c9970e27265fff7
photonnn <wasjiri@gmail.com>
2022-11-26 20:52:30 +0100

    Modify to do list

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './style.css';
 
   Change the way coordinates work from an array [x, y] to something more readable.
     # Maybe an object with x and y property
+    # Critical: Bug due to confusing the coordinates. Due to the nature of arrays
+      y will be present before x (arr[y][x]). This including the general confusing nature
+      of coordinates results in a need to prioritise this issue.
 
-  Add the notifation that calls out that the ship was or was nor succesfully placed.
+  Add the notification that calls out that the ship was or was nor succesfully placed.
     # We have tests for that. Might add an object to log things though.
 
-  Fix legalMoves functions to make a legalMoves based on the ship length
-    and the state of the board.
-    # Next on the menu
+  Done: Fix legalMoves functions to make a legalMoves based on the ship length
+          and the state of the board.
+          # Next on the menu.
 
 */
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,3 +1,5 @@
 console.log(Math.floor(Math.random() * 4) + 1);
 
 console.log(Array(4).fill(0).map(() => Array(4).fill(0)));
+
+console.log(Math.floor(Math.random() * 2));
