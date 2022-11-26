26836c1fbb82703f404c99be9ff315636af9d8b2
photonnn <wasjiri@gmail.com>
2022-11-26 20:55:11 +0100

    Add a test for AI Random Move

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,13 +1,28 @@
 import * as player from '../player';
+import * as gameboard from '../gameboard';
+import * as ship from '../ship';
+import { globalConsts } from '../root';
 
 describe('Player factory function', () => {
   let botOne;
+  let testBoard;
+  let testShipOne;
 
   beforeAll(() => {
     botOne = player.createPlayer('AI');
+    testBoard = gameboard.createGameboard(
+      globalConsts.TEST_BOARD_SIZE,
+      globalConsts.TEST_BOARD_SIZE,
+    );
+    testShipOne = ship.createShip(Math.floor(Math.random() * globalConsts.MAXIMUM_SHIP_LENGTH) + 1);
   });
 
-  test('Bot makes a legal random move', () => {
-    expect(botOne);
+  test.only('Bot makes a legal random move', () => {
+    expect(botOne.makeAIMove(testBoard, testShipOne)).not.toEqual([
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
   });
 });
